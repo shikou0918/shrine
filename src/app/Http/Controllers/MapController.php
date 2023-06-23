da9940572fde377d1eef6eabaeee093c26fc07b6
shikou0918 <shikou.0918@gmail.com>
2023-06-23 22:18:39 +0900

    pint実行

--- src/app/Http/Controllers/MapController.php
+++ src/app/Http/Controllers/MapController.php
@@ -7,6 +7,7 @@ class MapController extends Controller
     public function index()
     {
         $google_api_key = config('app.google_api_key');
+
         return view('map.index', compact('google_api_key'));
     }
 }
